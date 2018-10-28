d13ec130171cd252b05558d2a0a86d4d160d09e7
Felix Kugler <relgukxilef@gmail.com>
2018-10-28 18:08:07 +0100

    Avoid looking up materials by name every frame and object

--- Mesh.cpp
+++ Mesh.cpp
@@ -5,64 +5,64 @@
 
 #include "RessourceManager.h"
 
-Mesh::Mesh(MeshInfo meshInfo, glm::vec3 position, glm::vec3 diffuseColor, glm::vec3 emissionColor) :
-	meshInfo(meshInfo),
-    position(position),
-	diffuseColor(diffuseColor),
-    emissionColor(emissionColor)
-{
-}
-
 Mesh::Mesh(MeshInfo meshInfo, glm::mat4 model, glm::vec3 diffuseColor, glm::vec3 emissionColor) :
-    meshInfo(meshInfo),
-    model(model),
     diffuseColor(diffuseColor),
-    emissionColor(emissionColor)
+    emissionColor(emissionColor),
+    model(model),
+    vao(meshInfo.VAO),
+    count(meshInfo.numTriangles)
 {
+    auto material = RessourceManager::materials.at(meshInfo.materialName);
+    useDiffuseTexture = !material.diffuse_texname.empty();
+    useReflectionTexture = !material.metallic_texname.empty();
+    useNormalTexture = !material.normal_texname.empty();
+
+    if (useDiffuseTexture) {
+        diffuseTexture =
+            RessourceManager::textures.at(material.diffuse_texname);
+    }
+    if (useReflectionTexture) {
+        reflectionTexture =
+            RessourceManager::textures.at(material.metallic_texname);
+    }
+    if (useNormalTexture) {
+        normalTexture =
+            RessourceManager::textures.at(material.normal_texname);
+    }
 }
 
 void Mesh::draw(Shader &shader) {
-    auto& material = RessourceManager::materials[meshInfo.materialName];
-    if (material.diffuse_texname.length() > 0) {
+    if (useDiffuseTexture) {
         shader.setInteger("useDiffuseTex", 1);
         shader.setTexture2D(
-            "diffuseTex", GL_TEXTURE0,
-            RessourceManager::textures[material.diffuse_texname], 0
+            "diffuseTex", GL_TEXTURE0, diffuseTexture, 0
         );
     } else {
         shader.setInteger("useDiffuseTex", 0);
         shader.setVector3f(
-            "diffuseColor",
-            material.diffuse[0], material.diffuse[1], material.diffuse[2]
+            "diffuseColor", diffuseColor
         );
     }
-    shader.setTexture2D(
-        "reflectionTex", GL_TEXTURE1,
-        RessourceManager::textures[material.metallic_texname], 1
-    );
-    if (material.normal_texname.length() > 0) {
+    if (useReflectionTexture) {
+        shader.setTexture2D(
+            "reflectionTex", GL_TEXTURE1, reflectionTexture, 1
+        );
+    }
+    if (useNormalTexture) {
 		shader.setInteger("useNormalTex", 1);
         shader.setTexture2D(
-            "normalTex", GL_TEXTURE2,
-            RessourceManager::textures[material.normal_texname], 2
+            "normalTex", GL_TEXTURE2, normalTexture, 2
         );
 	} else {
 		shader.setInteger("useNormalTex", 0);
 	}
-    
-    //glm::mat4 modelMatrix(1.0f);
-    //modelMatrix = glm::translate(modelMatrix, position);
-	//shader.setMatrix4("model", modelMatrix);
+
     shader.setMatrix4("model", model);
 	shader.setVector3f("diffuseColor", diffuseColor);
     shader.setVector3f("emissionColor", emissionColor);
-    //shader.setVector3f(
-    //    "emissionColor",
-    //    material.emission[0], material.emission[1], material.emission[2]
-    //);
 
-	glBindVertexArray(meshInfo.VAO);
-	glDrawArrays(GL_TRIANGLES, 0, meshInfo.numTriangles);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, count);
 }
 
 Mesh::Mesh() {}
--- RessourceManager.h
+++ RessourceManager.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <map>
+#include <unordered_map>
 #include <string>
 #include <GL/glew.h>
 #include <tiny_obj_loader.h>
 
 class RessourceManager {
 public:
-    static std::map<std::string, tinyobj::material_t> materials;
-    static std::map<std::string, GLuint> textures;
+    static std::unordered_map<std::string, tinyobj::material_t> materials;
+    static std::unordered_map<std::string, GLuint> textures;
 };
