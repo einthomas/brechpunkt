adb1b6363c41ac3b00b44484aa0c05f61e1cab71
Felix Kugler <relgukxilef@gmail.com>
2019-01-11 16:04:01 +0100

    Reduce size of environment map

--- main.cpp
+++ main.cpp
@@ -221,7 +221,7 @@ int main(int argc, const char** argv) {
 
     GLuint environmentColor, environmentDepth;
     GLuint cubemapFramebuffer = generateFramebuffer(
-        256, 256, GL_TEXTURE_CUBE_MAP, {
+        128, 128, GL_TEXTURE_CUBE_MAP, {
             {GL_COLOR_ATTACHMENT0, environmentColor, GL_RGB16F},
             {GL_DEPTH_ATTACHMENT, environmentDepth, GL_DEPTH_COMPONENT16},
         }, {
@@ -230,7 +230,7 @@ int main(int argc, const char** argv) {
 
     GLuint blurredEnvironmentColor;
     GLuint blurredEnvironment = generateFramebuffer(
-        256, 256, GL_TEXTURE_CUBE_MAP, {
+        128, 128, GL_TEXTURE_CUBE_MAP, {
             {GL_COLOR_ATTACHMENT0, blurredEnvironmentColor, GL_RGB16F},
         }, {}
     );
@@ -769,7 +769,7 @@ int main(int argc, const char** argv) {
         glm::mat4 inverseProjectionMatrix = glm::inverse(projectionMatrix);
 
         glBindFramebuffer(GL_FRAMEBUFFER, cubemapFramebuffer);
-        glViewport(0, 0, 256, 256);
+        glViewport(0, 0, 128, 128);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         environmentShader.use();
         environmentShader.setMatrix4("view", glm::mat4(1.0f));
